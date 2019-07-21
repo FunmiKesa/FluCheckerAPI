78d411123637f9dcedd9c2ba837eb29316453029
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2019-07-21 22:47:39 +0100

    created an express and nodejs app to upload flu data to database

---
+++ routes/flu-data.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET users listing. */
+router.get("/", function(req, res, next) {
+  res.send("respond with a resource");
+});
+
+/*
+Post flu data
+*/
+router.post("/upload", function(req, res, next) {
+  res.send("Uploaded flu data.");
+});
+module.exports = router;
