797fa4a6f4e7aca74ad6728334b1698f2dc8b288
Oluwafunmilola Kesa <kesaoluwafunmilola@gmail.com>
2019-07-22 01:59:27 +0100

    implemented database connection

--- routes/flu-data.js
+++ routes/flu-data.js
@@ -1,15 +1,23 @@
 var express = require("express");
 var router = express.Router();
-var db =
-  /* GET users listing. */
-  router.get("/", function(req, res, next) {
-    res.send("respond with a resource");
-  });
+const FluService = require("../services/flu-service.js");
+
+let fluService = new FluService();
+
+/* GET users listing. */
+router.get("/", function(req, res, next) {
+  res.send("respond with a resource");
+});
+
+router.get("/initialize", function(req, res, next) {
+  fluService.createTable();
+});
 
 /*
 Post flu data
 */
 router.post("/upload", function(req, res, next) {
+  fluService.insertData();
   res.send("Uploaded flu data.");
 });
 module.exports = router;
--- services/flu-service.js
+++ services/flu-service.js
@@ -1,30 +1,44 @@
 var pg = require("pg");
 
 console.log(pg);
-var pool = new pg.Pool({
-  host: process.env.RDS_HOSTNAME,
-  user: process.env.RDS_USERNAME,
-  password: process.env.RDS_PASSWORD,
-  port: process.env.RDS_PORT
-});
 
-// connection using created pool
-pool.connect(function(err, client, done) {
-  if (err) {
-    console.error("Database connection failed: " + err.stack);
-    return;
+class FluService {
+  constructor() {}
+  connect() {
+    this.pool = new pg.Pool({
+      host: process.env.RDS_HOSTNAME,
+      user: process.env.RDS_USERNAME,
+      password: process.env.RDS_PASSWORD,
+      port: process.env.RDS_PORT
+    });
+
+    this.pool.connect(function(err, client, done) {
+      if (err) {
+        console.error("Database connection failed: " + err.stack);
+        return;
+      }
+      console.log("Connected to database.");
+    });
+  }
+  createTable() {
+    this.connect();
+    var query = this.pool.query(
+      "CREATE TABLE IF NOT EXISTS flu_data (id SERIAL PRIMARY KEY, hadFever BOOLEAN NOT NUL, hadCough BOOLEAN NOT NUL, temperature INTEGER DEFAULT(0)",
+      function(err, result) {
+        console.log(err, result);
+      }
+    );
   }
 
-  console.log("Connected to database.");
-});
-
-pool.end();
-
-export function insertData(data) {
-  var query = pool.query("INSERT INTO flu_data SET ?", data, function(
-    err,
-    result
-  ) {
-    console.log(err, result);
-  });
+  insertData(data) {
+    this.connect();
+    var query = this.pool.query("INSERT INTO flu_data SET ?", data, function(
+      err,
+      result
+    ) {
+      console.log(err, result);
+    });
+  }
 }
+
+module.exports = FluService;
